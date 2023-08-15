363e46e39aeb836bf999659a378f9a191db63d74
Alexey_Makeev <makeevhome@mail.ru>
2023-08-15 18:51:39 +0300

    update .env

--- project/libs/config/config-users/src/index.ts
+++ project/libs/config/config-users/src/index.ts
@@ -4,4 +4,3 @@ export { default as appConfig } from './lib/config/app.config';
 export * from './lib/get-mongoose-options';
 export { default as jwtConfig } from './lib/config/jwt.config';
 export * from './lib/get-jwt-options';
-export * from './lib/token-payload.interface';
