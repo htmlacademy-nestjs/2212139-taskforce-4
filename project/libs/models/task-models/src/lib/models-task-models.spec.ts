8419efd7876583c959a0ca8bcd2060bbcb1267bc
Alexey_Makeev <makeevhome@mail.ru>
2023-08-31 14:56:00 +0300

    fix bugs

--- project/libs/models/task-models/src/lib/models-task-models.spec.ts
+++
@@ -1,7 +0,0 @@
-import { modelsTaskModels } from './models-task-models';
-
-describe('modelsTaskModels', () => {
-  it('should work', () => {
-    expect(modelsTaskModels()).toEqual('models-task-models');
-  });
-});
